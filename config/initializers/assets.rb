1ee2922cb93da46d28f8715fd0a10d33cc0389c8
yuma829 <asketb03061225@gmail.com>
2021-06-26 19:06:44 +0900

    CSS適用箇所を制限
    CSSの適用ファイルを制限していなかったため，意図しないレイアウトの変更が起こってしまっていた.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,17 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w(
+    back.css
+    button.css
+    button2.css
+    list.css
+    sessions.css
+    slick.css
+    slider.css
+    works.css
+    admin/user.css
+    reset.css
+    not.css
+)
